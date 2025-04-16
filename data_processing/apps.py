6ffc571260e465e65bbdb1e25cf716172ac84934
TZC-JorgeLepasana <jlepasana@tzc.ie>
2025-04-16 09:48:22 +0800

    Update apps.py
    remove some codes

--- data_processing/apps.py
+++ data_processing/apps.py
@@ -1,10 +1,9 @@
 from django.apps import AppConfig
-import os  
+
 
 class DataProcessingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'data_processing'
-    path = os.path.join(os.path.dirname(os.path.abspath(__file__)))  # Explicit absolute path
     
     def ready(self):
         # Import models to prevent double registration
