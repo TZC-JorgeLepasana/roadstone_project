3ed09b87f6db441d6538c219497f7b6b186dcc80
TZC-JorgeLepasana <jlepasana@tzc.ie>
2025-04-16 09:11:03 +0800

    Update apps.py
    path add

--- data_processing/apps.py
+++ data_processing/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 class DataProcessingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'data_processing'
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)))  # Explicit absolute path
     
     def ready(self):
         # Import models to prevent double registration
